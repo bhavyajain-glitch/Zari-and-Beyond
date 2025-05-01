bb53c74061471ac04408bc5880d91c1e00abd57b
Bhavya Jain <bhavyajain@Bhavyas-MacBook-Pro.local>
2025-05-01 17:30:17 +0530

    Aboout us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/frontend_assets/assets'
+import Newsletter from '../components/Newsletter'
 
 const About = () => {
   return (
     <div>
-      
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1="ABOUT" text2="US" />
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt='about us' />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <h2 className='text-xl font-medium'>Welcome to our store!</h2>
+          <p className='text-sm text-gray-500'>
+            We are a passionate team dedicated to providing you with the best shopping experience. Our mission is to offer high-quality products at affordable prices, ensuring that you find exactly what you're looking for.</p>
+          <p className='text-sm text-gray-500'>
+            Thank you for choosing us. We look forward to serving you!</p>
+
+      </div>
+      </div>
+      <div className='text-xl py-4'>
+        <Title text1={"WHY"} text2={"CHOOSE US?"} />
+      </div>
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>We prioritize quality in every product we offer. Our team carefully selects items to ensure they meet our high standards.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience:</b>
+          <p className='text-gray-600'>We prioritize quality in every product we offer. Our team carefully selects items to ensure they meet our high standards.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>We prioritize quality in every product we offer. Our team carefully selects items to ensure they meet our high standards.</p>
+        </div>
+      </div>
+      <Newsletter />
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [currentState, setCurrentState]  = useState('Login')
+  const onSubmitHandler = async(event)=>{
+    event.preventDefault()
+  }
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p className='prata-regular text-3xl text-gray-900'>{currentState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-900' />
+      </div>
+      {currentState === 'Login'? '' :<input type='text' className='w-full px-3 py-2 border border-gray-800' placeholder='Name' />}
+      <input type='email' className='w-full px-3 py-2 border border-gray-800' placeholder='Email' />
+      <input type='password' className='w-full px-3 py-2 border border-gray-800' placeholder='Password' />
+      <div className='w-full flex justify-between text-sm mt-[-2%]'>
+        <p className='text-gray-500 cursor-pointer'>Forgot Password?</p>
+        {
+          currentState === 'Login' ? 
+          <p onClick={() => setCurrentState('Sign Up')} className='text-gray-500 cursor-pointer'>Create an account</p> : 
+          <p onClick={() => setCurrentState('Login')} className='text-gray-500 cursor-pointer'>Already have an account?</p>
+        }
+      </div>
+      <button className='bg-[#7d6c58] text-white font-light px-8 py-2 mt-4'>{currentState === 'Login'? 'Sign In' : 'Sign Up'}</button>
+    </form>
   )
 }
 
